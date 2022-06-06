dd8b5dc3215de9e505f9797e5b090a78b8a6a277
matotec <matias.chaile.1988@gmail.com>
2022-06-06 02:21:34 -0300

    agregado storage services

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -4,8 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-
+  
   constructor() { }
 
-  
+  addComment(comment: Comment, id: number) {
+    let data = this.getComment(id);
+    data ? data.push(comment) : (data = [comment]);
+    this.setComment(id,data);
+  }
+
+  setComment<t>(id: number, data: t) {
+    localStorage.setItem(id.toString(), JSON.stringify(data));
+  }
+
+  getComment(id: number): Comment[] {
+    let data = localStorage.getItem(id.toString());
+
+    return data ? JSON.parse(data) : [];
+  }
+
+  deleteComment(id: number) {
+    localStorage.removeItem(id.toString());
+  }
 }
