256b2cf3ac6562cecc3a98ae4c1b977c49186cf9
matotec <matias.chaile.1988@gmail.com>
2022-06-08 00:19:22 -0300

    fix deploy v20

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-interview'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/angular-interview/index.html'));
+app.use(express.static('./dist/angular-interview'));
+app.get('/*',function(req,res){
+    res.sendFile('index.html',{root : '/dist/angular-interview/'});
 });
 
 app.listen(process.env.PORT || 8080);
